7edd0618dd1120790929e51da00d3cc67db13372
CL Roberts <clr2112@uw.edu>
2025-06-11 15:04:30 -0700

    add random effects for slope and intercept

--- logreg.r
+++ logreg.r
@@ -18,8 +18,6 @@ seeds <- sample(1:1e4, size = chains)
 iter <- 1000
 warmup <- 500
 control <- list()
-# control <- list(adapt_delta = 0.95)
-
 
 ## set up ---
 
@@ -27,6 +25,7 @@ control <- list()
 library(TMB)
 library(tmbstan)
 library(here)
+library(ggplot2)
 
 # dir 
 
@@ -36,30 +35,59 @@ dir_src <- here("src")
 ## simulate data ---
 
 set.seed(406)
-n <- 100
+n <- 50              # number samples per random effect
+num_groups <- 10      # number of groups in random effect
 
 # regressors
 
-x <- rnorm(n)
-beta0 <- 1
-beta1 <- 2
+x <- cbind(1, rnorm(n*num_groups), rep(1:num_groups, rep(n, num_groups)))
+
+# parameters 
+mu <- c(0, 5)         # mean of random intercept and slope
+sigma <- c(1, 3)     # variance of random intercept and slope
+
+beta <- cbind(
+    rnorm(num_groups, mean = mu[1], sd = sigma[1]), 
+    rnorm(num_groups, mean = mu[2], sd = sigma[2])
+)
 
 # response
 
-logit_p <- beta0 + beta1*x + rnorm(n, sd = .1)
+logit_p <- c()
+for (group in 1:num_groups) {
+    logit_p <- c(
+        logit_p, 
+        x[x[,3] == group,1:2] %*% beta[group,] + rnorm(n, sd = 1)
+    )
+}
+
 p <- 1/(1+exp(-logit_p))
 
-size <- rep(10, n)
-y <- rbinom(n = n, size = size, p = p)
+size <- rep(10, n*num_groups)
+y <- rbinom(n = n*num_groups, size = size, p = p)
+
+## data ----
+
+da <- list(y = y, x = x[,2], size = size, group = x[,3])
 
 # priors
 
-beta1_prior <- c(10, 1) # dnorm - mean, sd
-beta2_prior <- c(10, 1)
+beta0_prior <- c(0, 3) # dnorm - mean, sd
+beta1_prior <- c(0, 3)
+mu0_prior <- c(0, 3) 
+sigma0_prior <- c(1, 3) 
+mu1_prior <- c(5, 3) 
+sigma1_prior <- c(3, 3) 
 
-da <- list(y = y, x = x, size = size, 
-           beta1_prior = beta1_prior,
-           beta2_prior = beta2_prior)
+priors <- list(
+    beta0_prior = beta0_prior, beta1_prior = beta1_prior, 
+    mu0_prior = mu0_prior, sigma0_prior = sigma0_prior, 
+    mu1_prior = mu1_prior, sigma1_prior = sigma1_prior
+)
+
+# all inputs
+
+input <- c(da, priors)
 
 ## Compile and load model ---
 if("logreg" %in% names(getLoadedDLLs())) {
@@ -68,16 +96,22 @@ if("logreg" %in% names(getLoadedDLLs())) {
 compile(here(dir_src, "logreg.cpp"))
 dyn.load(dynlib(here(dir_src, "logreg")))
 
-## initial parameter values ---
-inits <- list(beta = c(2, 2))
+## parameters ---
+parameters <- list(
+    beta0 = rep(1, num_groups), beta1 = rep(1, num_groups), mu = c(3, 3), sigma = c(3, 3)
+)
+
+# fixed parameters
+# map <- list(sigma = factor(NA), beta1 = rep(factor(NA), num_groups))
 
 ## create model object ---
 
 obj <- MakeADFun(
-    data = da,
-    parameters = inits,
-    DLL = "logreg",
-    hessian = TRUE
+    data = input,
+    parameters = parameters,
+    # map = map,
+    random = "beta1",
+    DLL = "logreg"
 )
 
 ## save initial fit ---
@@ -95,8 +129,9 @@ opt$par
 sdreport(obj)
 
 ## run NUTS ---
+
 mcmc_start_time <- Sys.time()
-fit <- tmbstan(obj, chains = chains, cores = chains, init = inits, iter = iter, 
+fit <- tmbstan(obj, chains = chains, cores = chains, init = unlist(parameters), iter = iter, 
                 warmup = warmup, seed = seeds, algorithm = "NUTS", silent = FALSE,
                 control = control)
 mcmc_end_time <- Sys.time()
@@ -113,10 +148,9 @@ apply(mcmc_results, MARGIN = 2, FUN = sd)
 hist(mcmc_results[,1])
 traceplot(fit, pars=names(obj$par), inc_warmup=TRUE)
 
-
 # plot fit ---
 
-plot_data <- data.frame(x = x, y = y)
+plot_data <- data.frame(x = x[,2], y = y, group = factor(x[,3]))
 
 yhat_post <- apply(
         mcmc_results,
@@ -129,16 +163,12 @@ plot_data$yhat <- apply(yhat_post, MARGIN = 2, FUN = median)
 plot_data$yhat_lwr <- apply(yhat_post, MARGIN = 2, FUN = \(x) quantile(x, .025))
 plot_data$yhat_upr <- apply(yhat_post, MARGIN = 2, FUN = \(x) quantile(x, .975))
 
-plot_data$y_true <- obj$report(c(beta0, beta1))$yhat
-
-plot_data <- plot_data[order(plot_data$x),]
-
-plot(plot_data$x, plot_data$y)
-lines(plot_data$x, plot_data$yhat, type = "l")
-lines(plot_data$x, plot_data$y_true, type = "l", col = "red")
-lines(plot_data$x, plot_data$yhat_lwr, type = "l", lty = 2)
-lines(plot_data$x, plot_data$yhat_upr, type = "l", lty = 2)
-
+ggplot(data = plot_data, aes(x = x)) +
+    geom_point(aes(y = y, color = group)) +
+    geom_line(aes(y = yhat, color = group)) +
+    geom_ribbon(aes(ymin = yhat_lwr, ymax = yhat_upr, fill = group), alpha = .1) +
+    theme_bw() +
+    theme(legend.position = "none")
 
 # test for overdispersion ---
 
@@ -153,4 +183,4 @@ phat <- apply(phat_post, MARGIN = 2, FUN = median)
 
 chi_sq <- sum((y - size*phat)^2 / (size*phat*(1-phat)))
 
-pchisq(chi_sq, n - ncol(mcmc_results), lower.tail = FALSE)
+pchisq(chi_sq, n*num_groups - ncol(beta), lower.tail = FALSE)
--- src/logreg.cpp
+++ src/logreg.cpp
@@ -23,39 +23,63 @@ Type objective_function<Type>::operator() ()
   DATA_VECTOR(y);
   DATA_VECTOR(x);
   DATA_VECTOR(size);
+  DATA_FACTOR(group);
 
   // priors
-  DATA_VECTOR(beta1_prior); 
-  DATA_VECTOR(beta2_prior);
+  DATA_VECTOR(beta0_prior);    
+  DATA_VECTOR(beta1_prior);
+  DATA_VECTOR(mu0_prior); 
+  DATA_VECTOR(sigma0_prior); 
+  DATA_VECTOR(mu1_prior); 
+  DATA_VECTOR(sigma1_prior); 
 
   // parameters
-  // PARAMETER(p);
-  PARAMETER_VECTOR(beta);
+  PARAMETER_VECTOR(beta0);
+  PARAMETER_VECTOR(beta1);
+  PARAMETER_VECTOR(mu);
+  PARAMETER_VECTOR(sigma);
   
   // model objects
   int n = y.size();
+  int num_groups = beta1.size();
+  int j;
+
   vector<Type> logit_phat(n);
   vector<Type> phat(n);
   vector<Type> yhat(n);
-
-  // model
-  for (int i = 0; i < n; i++) {
-    logit_phat(i) = beta(0) + beta(1)*x(i);
-    phat(i) = 1 / (1 + exp(-logit_phat(i)));
-    yhat(i) = size(i) * phat(i);
-  }
   
   // objective function
   Type negLogLik = 0.0;
 
+  // random effect
+  for (int j = 0; j < num_groups; j++) {
+    negLogLik -= dnorm(beta0(j), mu(0), sigma(0), true);
+    negLogLik -= dnorm(beta1(j), mu(1), sigma(1), true);
+  }
+
+  // model
+  for (int i = 0; i < n; i++) {
+    j = group(i)-1;
+    logit_phat(i) = beta0(j) + beta1(j)*x(i);
+    phat(i) = 1 / (1 + exp(-logit_phat(i)));
+    yhat(i) = size(i) * phat(i);
+  }  
+  
+  // likelihood
   for (int i = 0; i < n; i++) {
     negLogLik -= y(i) * logit_phat(i);
     negLogLik += size(i)*log(1 + exp(logit_phat(i)));
   }
 
   // priors
-  negLogLik -= dnorm(beta(0), beta1_prior(0), beta1_prior(1), true);
-  negLogLik -= dnorm(beta(1), beta2_prior(0), beta2_prior(1), true);
+  for (int j = 0; j < num_groups; j++) {
+    negLogLik -= dnorm(beta0(j), beta0_prior(0), beta0_prior(1), true);
+    negLogLik -= dnorm(beta1(j), beta1_prior(0), beta1_prior(1), true);
+  }
+  negLogLik -= dnorm(mu(0), mu0_prior(0), mu0_prior(1), true);
+  negLogLik -= dnorm(sigma(0), sigma0_prior(0), sigma0_prior(1), true);
+  negLogLik -= dnorm(mu(1), mu1_prior(0), mu1_prior(1), true);
+  negLogLik -= dnorm(sigma(1), sigma1_prior(0), sigma1_prior(1), true);
   
   // report
   REPORT(yhat);
